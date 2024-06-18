2afc34abaab3b9f9d69177cd5c05cbc7dd1e487d
Alex Feldman <79878358+afeldmanSCSU@users.noreply.github.com>
2024-06-18 12:26:41 -0400

    Update __init__.py (Updated method to ensure pet pictures directory exists)

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
+import os
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -16,12 +17,16 @@ mail = Mail()
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
-
+    
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     migrate.init_app(app, db)
     mail.init_app(app)
+
+    # Ensure the pet pictures directory exists
+    from app.utils import ensure_directory_exists
+    ensure_directory_exists(os.path.join(app.root_path, 'static/pet_pics'))
 
     from app import routes
     app.register_blueprint(routes.bp)
