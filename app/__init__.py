1d28ced62077a5595b26ce3b48a0426010c0aece
Alex Feldman <79878358+afeldmanSCSU@users.noreply.github.com>
2024-06-30 17:02:07 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
+import logging
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
+from apscheduler.schedulers.background import BackgroundScheduler
 import os
+
+# Set up logging for APScheduler
+logging.basicConfig()
+logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -13,6 +19,7 @@ login_manager.login_view = 'main.login'
 login_manager.login_message_category = 'info'
 migrate = Migrate()
 mail = Mail()
+scheduler = BackgroundScheduler()
 
 def create_app():
     app = Flask(__name__)
@@ -31,4 +38,9 @@ def create_app():
     from app import routes
     app.register_blueprint(routes.bp)
 
+    if not scheduler.running:
+        scheduler.start()
+
     return app
+
+app = create_app()
