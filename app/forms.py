29b47e0b8a0bd57bdc9818c197bd4e4a32d63706
Alex Feldman <79878358+afeldmanSCSU@users.noreply.github.com>
2024-07-22 16:40:04 -0400

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, FileField, TextAreaField, DateField, TimeField, SelectField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from app.models import User
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, FileField, TextAreaField, DateField, TimeField, SelectField, DecimalField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Optional
+from app.models import User, Pet, VaccineRecord
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -28,6 +28,7 @@ class LoginForm(FlaskForm):
 
 class AddPetForm(FlaskForm):
     name = StringField('Pet Name', validators=[DataRequired()])
+    gender = SelectField('Gender', choices=[('Male', 'Male'), ('Female', 'Female')], validators=[DataRequired()])
     age = IntegerField('Age', validators=[DataRequired()])
     breed = StringField('Breed', validators=[DataRequired()])
     picture = FileField('Upload Picture', validators=[DataRequired()])
@@ -40,6 +41,13 @@ class PetHistoryForm(FlaskForm):
     description = TextAreaField('Description', validators=[DataRequired()])
     submit = SubmitField('Save')
 
+class EditPetForm(FlaskForm):
+    name = StringField('Pet Name', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired()])
+    breed = StringField('Breed', validators=[DataRequired()])
+    gender = SelectField('Gender', choices=[('Male', 'Male'), ('Female', 'Female')], validators=[DataRequired()])
+    picture = FileField('Upload Picture')
+
 class VetAppointmentForm(FlaskForm):
     pet = SelectField('Select Pet', validators=[DataRequired()], coerce=int)
     date = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()])
@@ -48,3 +56,101 @@ class VetAppointmentForm(FlaskForm):
     description = TextAreaField('Description', validators=[DataRequired()])
     submit = SubmitField('Save')
 
+class VaccineTrackingForm(FlaskForm):
+    pet = SelectField('Choose Pet', coerce=int, validators=[DataRequired()])
+    vaccine_type = SelectField('Choose Vaccine Type', choices=[
+        ('rabies', 'Rabies'),
+        ('distemper', 'Distemper'),
+        ('parvovirus', 'Parvovirus'),
+        ('hepatitis', 'Hepatitis'),
+        ('leptospirosis', 'Leptospirosis'),
+        ('bordetella', 'Bordetella'),
+        ('lyme', 'Lyme'),
+        ('canine_influenza', 'Canine Influenza'),
+        ('other', 'Other')
+    ], validators=[DataRequired()])
+    other_vaccine = StringField('Other Vaccine')
+    date_administered = DateField('Date Administered', validators=[DataRequired()])
+    next_due_date = DateField('Next Due Date', validators=[DataRequired()])
+    submit = SubmitField('Save')
+
+    def __init__(self, *args, **kwargs):
+        super(VaccineTrackingForm, self).__init__(*args, **kwargs)
+        self.pet.choices = [(pet.id, pet.name) for pet in Pet.query.all()]
+
+class EditVaccineForm(FlaskForm):
+    vaccine_type = SelectField('Choose Vaccine Type', choices=[
+        ('rabies', 'Rabies'),
+        ('distemper', 'Distemper'),
+        ('parvovirus', 'Parvovirus'),
+        ('hepatitis', 'Hepatitis'),
+        ('leptospirosis', 'Leptospirosis'),
+        ('bordetella', 'Bordetella'),
+        ('lyme', 'Lyme'),
+        ('canine_influenza', 'Canine Influenza'),
+        ('other', 'Other')
+    ], validators=[DataRequired()])
+    other_vaccine = StringField('Other Vaccine')
+    date_administered = DateField('Date Administered', validators=[DataRequired()])
+    next_due_date = DateField('Next Due Date', validators=[DataRequired()])
+    submit = SubmitField('Update')
+
+class FeedingScheduleForm(FlaskForm):
+    pet = SelectField('Select Pet', coerce=int, validators=[DataRequired()])
+    food_type = StringField('Type of Food', validators=[DataRequired()])
+    feedings_per_day = IntegerField('Feedings per Day', validators=[DataRequired()])
+    food_amount = SelectField('Food Amount (cups)', choices=[(str(i/4), f"{i/4} cups") for i in range(21)], validators=[DataRequired()])
+    feeding_time_1 = TimeField('Feeding Time 1', format='%H:%M', validators=[Optional()])
+    feeding_time_2 = TimeField('Feeding Time 2', format='%H:%M', validators=[Optional()])
+    feeding_time_3 = TimeField('Feeding Time 3', format='%H:%M', validators=[Optional()])
+    feeding_time_4 = TimeField('Feeding Time 4', format='%H:%M', validators=[Optional()])
+    feeding_time_5 = TimeField('Feeding Time 5', format='%H:%M', validators=[Optional()])
+    submit = SubmitField('Save')
+
+    def __init__(self, *args, **kwargs):
+        super(FeedingScheduleForm, self).__init__(*args, **kwargs)
+        self.pet.choices = [(pet.id, pet.name) for pet in Pet.query.all()]
+
+class EditFeedingScheduleForm(FlaskForm):
+    pet = SelectField('Select Pet', coerce=int, validators=[DataRequired()])
+    food_type = StringField('Type of Food', validators=[DataRequired()])
+    feedings_per_day = IntegerField('Feedings per Day', validators=[DataRequired()])
+    feeding_time_1 = TimeField('Feeding Time 1', format='%H:%M', validators=[Optional()])
+    feeding_time_2 = TimeField('Feeding Time 2', format='%H:%M', validators=[Optional()])
+    feeding_time_3 = TimeField('Feeding Time 3', format='%H:%M', validators=[Optional()])
+    feeding_time_4 = TimeField('Feeding Time 4', format='%H:%M', validators=[Optional()])
+    feeding_time_5 = TimeField('Feeding Time 5', format='%H:%M', validators=[Optional()])
+    submit = SubmitField('Update')
+
+    def __init__(self, *args, **kwargs):
+        super(EditFeedingScheduleForm, self).__init__(*args, **kwargs)
+        self.pet.choices = [(pet.id, pet.name) for pet in Pet.query.all()]
+
+class ExpenseForm(FlaskForm):
+    pet = SelectField('Choose Pet', coerce=int, validators=[DataRequired()])
+    expense_type = SelectField('Expense Type', choices=[
+        ('Appointments', 'Appointments'),
+        ('Vaccination', 'Vaccination'),
+        ('Food', 'Food'),
+        ('Medication', 'Medication'),
+        ('Grooming', 'Grooming'),
+        ('Toy and Entertainment', 'Toy and Entertainment'),
+        ('Training', 'Training'),
+        ('Insurance', 'Insurance'),
+        ('Pet Sitting', 'Pet Sitting'),
+        ('Travel', 'Travel'),
+        ('Registration', 'Registration'),
+        ('Accessories', 'Accessories'),
+        ('Miscellaneous Supplies', 'Miscellaneous Supplies'),
+        ('Dietary Supplements', 'Dietary Supplements'),
+        ('Subscriptions', 'Subscriptions'),
+        ('Events', 'Events'),
+        ('Other', 'Other')
+    ], validators=[DataRequired()])
+    amount = DecimalField('Amount', validators=[DataRequired()])
+    date = DateField('Date Paid', format='%Y-%m-%d', validators=[DataRequired()])
+    submit = SubmitField('Save')
+
+    def __init__(self, *args, **kwargs):
+        super(ExpenseForm, self).__init__(*args, **kwargs)
+        self.pet.choices = [(pet.id, pet.name) for pet in Pet.query.all()]
